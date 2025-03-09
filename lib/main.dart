5d358345d4f712d042c04e587d3d3df069be5bd7
moFiras1 <mohammedFirasAlJallad@gmail.com>
2025-03-09 14:19:34 +0200

    end the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:rick_and_morty/modules/home/controller/home_controller.dart';
 
 import 'core/app_router.dart';
+import 'core/firebase_notifications/firebase_notifications.dart';
+import 'firebase_options.dart';
 import 'modules/char_details/controller/char_details_controller.dart';
 
 final navigationKey = GlobalKey<NavigatorState>();
 
-void main() {
-  runApp(
-      MultiProvider(providers: [
-        ChangeNotifierProvider(create: (_) => HomeController()),
-        ChangeNotifierProvider(create: (_) => CharDetailsController()),
-      ]
-        ,child: const MyApp(), // Register Controller])
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // FirebaseNotifications().initNotification();
+  FirebaseMessaging.onBackgroundMessage(
+      FirebaseNotifications.instance.firebaseMessagingBackgroundHandler);
+  await FirebaseNotifications.instance.setupFlutterNotifications();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => HomeController()),
+      ChangeNotifierProvider(create: (_) => CharDetailsController()),
+    ],
+    child: const MyApp(), // Register Controller])
   ));
-  }
+}
 
 class MyApp extends StatelessWidget {
   static final GlobalKey<NavigatorState> navigationKey = GlobalKey<NavigatorState>();
